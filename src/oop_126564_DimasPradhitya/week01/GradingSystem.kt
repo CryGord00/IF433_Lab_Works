35131577ff04b92a61a009912f294b15b9847a66
CryGord00 <dimaspradhitya73@gmail.com>
2026-02-04 16:11:39 +0700

    Week01: initial setup and pricedural coe

---
+++ src/oop_126564_DimasPradhitya/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_126564_DimasPradhitya.week01
+
+fun main() {
+    var name: string = "Dimas Pradhitya"
+    var score: Int = 95
+
+    // Concatennation gaya lama
+    println("Nama: " + name + ", Nilai:"+ score)
+}
