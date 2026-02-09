0c01ba7924dda027a290cbdd9e63a1230e08b649
CryGord00 <dimaspradhitya73@gmail.com>
2026-02-09 21:44:33 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_126564_DimasPradhitya/week01/CircleCalculator.kt
+++ src/oop_126564_DimasPradhitya/week01/CircleCalculator.kt
@@ -1,23 +1,17 @@
 package oop_126564_DimasPradhitya.week01
 
-fun main(args: Array<String>){
-    //Variabel definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
 
-    //Caculation
     val area = pi * radius * radius
 
-    //Output Concentation
-    println("Radius:  $radius, Area: $area)
-
-    //Logic check
+    println("Radius: $radius, Area: $area")
     println(checkSize(area))
 }
 
-fun ChechkSize(area: Double) = if (area > 100) {
-    "this is a Big Circle"
-    } ese {
-        "This is a Small Circle"
-    }
+fun checkSize(area: Double) = if (area > 100) {
+    "This is a Big Circle"
+} else {
+    "This is a Small Circle"
 }
