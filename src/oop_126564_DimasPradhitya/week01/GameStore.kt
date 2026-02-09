56a82e0da4fc1c45868405cebb163256b65c9391
CryGord00 <dimaspradhitya73@gmail.com>
2026-02-09 20:41:10 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_126564_DimasPradhitya/week01/GameStore.kt
+++ src/oop_126564_DimasPradhitya/week01/GameStore.kt
@@ -1,8 +1,12 @@
 package oop_126564_DimasPradhitya.week01
 
-package oop_126564_DimasPradhitya.week01
-
 fun main() {
     val gameTitle = "GTA San Andreas"
     val price = 550000
+}
+
+fun calculationDiscount(price: int): Int = if (price > 550000) {
+    (price 8 0.8).toInt()
+} else {
+    (price * 0.9).toInt()
 }
