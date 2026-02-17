0b4e2ac43d8eb4aa301e6300827067587996f9c8
CryGord00 <dimaspradhitya73@gmail.com>
2026-02-17 20:56:51 +0700

    Week02: Implement selection Logic for constructors

--- src/oop_126564_DimasPradhitya/week02/Main.kt
+++ src/oop_126564_DimasPradhitya/week02/Main.kt
@@ -1,28 +1,40 @@
 package oop_126564_DimasPradhitya.week02
 
 import java.util.Scanner
+
 fun main() {
-    val scanner = Scaneer(source = System. 'in')
+    val scanner = Scanner(System.`in`)
 
     println("--- APLIKASI PMB UMN ---")
-
-    println("Masukan Nama: ")
+    print("Masukkan Nama: ")
     val name = scanner.nextLine()
 
-    print("Masukan NIM (Wajib 5 Karakter): ")
+    print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
 
-    scanner.nextLine() //Bersihkan buffer newline penyakit klasik Scanner!)
+    scanner.nextLine() // Bersihkan buffer newline
 
-    //Validasi di sisi pemanggil (Main)
-    if (nim.length !=5){
-       println("EROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
-         println("Masukan jurusan: ")
-         val major = scanner.nextline()
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
 
-         // Instansiasi objek karena data sudah aman
-         val sl = Student(name, nim, major)
-         println("Status: Pendaftaran Selesai.")
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+
+            val s1 = Student(name, nim, major)
+            // Bagian ini akan merah kalau Student.kt belum diupdate
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+
+        } else if (type == 2) {
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }
