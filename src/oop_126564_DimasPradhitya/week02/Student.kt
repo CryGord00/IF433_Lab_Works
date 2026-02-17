f539ab394592e3b670efd783f9067b0bd0c1ef54
CryGord00 <dimaspradhitya73@gmail.com>
2026-02-17 20:34:57 +0700

    Week02: challenge refactorr primary constructorr using default args

--- src/oop_126564_DimasPradhitya/week02/Student.kt
+++ src/oop_126564_DimasPradhitya/week02/Student.kt
@@ -1,17 +1,21 @@
 package oop_126564_DimasPradhitya.week02
 
-class Student( val name: String, val nim: String, var major: String){
-   init {
-       //Validasi Sederhana: Cek panjang NIM
-       if (nim.length != 5){
-           println("WARNING: Objek tercipta dengan NIM (%nim) yang tidak valid!")
-           println("DATA mahasiswa $name mungkin akan bermasalah di sistem.")
-       } else {
-           println("LOG: Objek Student $name berhasil dialokasikan di Memory .")
-       }
-   }
-
-    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
-        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String,
+    var gpa: Double = 0.0
+) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!") [cite: 31, 32]
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.") [cite: 35]
+        }
+    }
+    // Secondary Constructor
+    // Wajib memanggil Primary Constructor menggunakan "this()"
+    constructor(name: String, nim: String) : this(name, nim, major ="Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).") [cite: 49, 52]
     }
 }
