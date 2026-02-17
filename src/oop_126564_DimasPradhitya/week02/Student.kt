7f3b89c130afc8fdc21182fae64b9d8e17fce416
CryGord00 <dimaspradhitya73@gmail.com>
2026-02-17 20:22:18 +0700

    Week02: add secondary constructor

--- src/oop_126564_DimasPradhitya/week02/Student.kt
+++ src/oop_126564_DimasPradhitya/week02/Student.kt
@@ -11,4 +11,7 @@ class Student( val name: String, val nim: String, var major: String){
        }
    }
 
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 }
