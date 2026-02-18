2ff2f93d3dd47bb94d9bc41b278b78bab806aed3
CryGord00 <dimaspradhitya73@gmail.com>
2026-02-18 15:56:30 +0700

    week03: add private property

--- src/oop_126564_DimasPradhitya/week03/Employee.kt
+++ src/oop_126564_DimasPradhitya/week03/Employee.kt
@@ -4,10 +4,24 @@ class Employee(val name: String) {
     var salary: Int = 0
         set(value) {
             if (value < 0) {
-            println("Eror: Gaji tidak boleh negatif! Di-set ke 0.")
-            field = 0 // Gunakan field, bukan this.salary
+                println("Eror: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0 // Gunakan field, bukan this.salary
             } else {
                 field = value
             }
         }
+
+    //Hanya bis diakses di file/class ini saja
+    private var performanceRating: Int = 3
+
+    fun increasePerformace() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    // Kita tidak buat getter untuk performanceRating. jadi data ini benar-benar rahasia
+// Kecuali kita buat function khusus utnuk print.
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
