42f955035447efafd91eafad0c2cc2ed0f747405
CryGord00 <dimaspradhitya73@gmail.com>
2026-02-21 11:38:56 +0700

    week03: (task 1) implement Weapon testing in Main

--- src/oop_126564_DimasPradhitya/week03/Main.kt
+++ src/oop_126564_DimasPradhitya/week03/Main.kt
@@ -1,17 +1,28 @@
 package oop_126564_DimasPradhitya.week03
 
 fun main() {
-    val e = Employee.("Budi")
+    // --- TESTING TUGAS 1: WEAPON ---
+    println("=== TESTING WEAPON ===")
+    val mySword = Weapon("Excalibur")
 
-    // 1. Test Validasi Salary
-    e.salary = -1000 // Harusnya print Eror
-    e.salary = 5000000
-    println("gaji: ${e.salary}")
+    // Sesuai Instruksi: Set damage ke -50 (harus gagal/tidak berubah)
+    mySword.damage = -50
 
-    // 2. Test Encapsulation
-    e.increasePerformance()
-    // e.increasePerformance = 5 // Coba uncomment, pasti merah (Eror)
+    // Sesuai Instruksi: Set damage ke 9999 (harus jadi 1000)
+    mySword.damage = 9999
 
-    // 3> test Computed Property
-    println("Pajak yang harus dibayar: ${e.tax}")
+    // Cetak Tier (Harus muncul "Legendary" karena damage 1000)
+    println("Senjata: ${mySword.name} | Damage: ${mySword.damage} | Tier: ${mySword.tier}")
+    println("-----------------------")
+
+    // --- TESTING TUGAS 2: PLAYER ---
+    println("\n=== TESTING PLAYER ===")
+    val player1 = Player("Dimas_Pro")
+
+    // Sesuai Instruksi: Panggil addXp(50) -> Masih Level 1
+    player1.addXp(50)
+
+    // Sesuai Instruksi: Panggil addXp(60) -> Total 110, harus Level Up ke 2
+    player1.addXp(60)
+}
 }
