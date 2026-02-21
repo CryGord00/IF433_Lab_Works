87e137de5dceb61f4850913365a13291469bec3c
CryGord00 <dimaspradhitya73@gmail.com>
2026-02-21 11:03:14 +0700

    week03: (task 1) create Weapon class with validation

--- src/oop_126564_DimasPradhitya/week03/Weapon.kt
+++ src/oop_126564_DimasPradhitya/week03/Weapon.kt
@@ -1,2 +1,27 @@
 package oop_126564_DimasPradhitya.week03
 
+class Weapon(val name: String) {
+    // Properti damage dengan Custom Setter (Validasi)
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Peringatan: Damage tidak boleh negatif! Nilai tetap $field.")
+                // Tidak mengubah field jika input negatif
+            } else if (value > 1000) {
+                println("Peringatan: Damage terlalu OP! Dipaksa ke maksimal 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    // Computed Property untuk menentukan Tier senjata berdasarkan damage
+    val tier: String
+        get() {
+            return when {
+                damage > 800 -> "Legendary"
+                damage > 500 -> "Epic"
+                else -> "Common"
+            }
+        }
+}
