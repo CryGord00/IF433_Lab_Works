981731844df0d0638edad5558f3996bd6b046ec3
CryGord00 <dimaspradhitya73@gmail.com>
2026-03-11 12:30:46 +0700

    Week 06: create compound interfaces

---
+++ src/oop_126564_DimasPradhitya/Week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_126564_DimasPradhitya.Week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+       fun connectToBluetooth()
+}
+
+interface Rechargeable {
+       fun rechargeBattery()
+}
