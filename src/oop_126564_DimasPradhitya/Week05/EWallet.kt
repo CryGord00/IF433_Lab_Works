c14ef86866b42515f38ccf1b9a93b42508d0f98e
CryGord00 <dimaspradhitya73@gmail.com>
2026-03-11 15:04:37 +0700

    Week 06: create PaymentMethoda contract

--- src/oop_126564_DimasPradhitya/Week05/EWallet.kt
+++ src/oop_126564_DimasPradhitya/Week05/EWallet.kt
@@ -1,17 +1,17 @@
-package oop_126564_DimasPradhitya.Week05
-
-class EWallet (accountName: String, var balance: Double) : PaymentMethod(accountName) {
-    override fun processPayment(amount: Double) {
-        if (balance >= amount) {
-            balance -= amount
-            println("[$accountName - E-Wallet] Pembayaran Rp $amount Berhasil. Sisa saldo: Rp $balance")
-        }else {
-            println("[$accountName - EWallet] Saldo tidak cukup untuk membayar Rp $amount")
-        }
-    }
-
-    fun topUp(amount: Doube) {
-        balance -= amount
-        println("[$accountName - EWallet] Top up Rp $amount berhasil. Saldo sekarang: Rp $balance")
-    }
-}
+//package oop_126564_DimasPradhitya.Week05
+//
+//class EWallet (accountName: String, var balance: Double) : PaymentMethod(accountName) {
+//    override fun processPayment(amount: Double) {
+//        if (balance >= amount) {
+//            balance -= amount
+//            println("[$accountName - E-Wallet] Pembayaran Rp $amount Berhasil. Sisa saldo: Rp $balance")
+//        }else {
+//            println("[$accountName - EWallet] Saldo tidak cukup untuk membayar Rp $amount")
+//        }
+//    }
+//
+//    fun topUp(amount: Doube) {
+//        balance -= amount
+//        println("[$accountName - EWallet] Top up Rp $amount berhasil. Saldo sekarang: Rp $balance")
+//    }
+//}
---
+++ src/oop_126564_DimasPradhitya/Week06/PaymentMethod.kt
@@ -0,0 +1,5 @@
+package oop_126564_DimasPradhitya.Week06
+
+interface Payment {
+    fun pay(amount: Double)
+}
