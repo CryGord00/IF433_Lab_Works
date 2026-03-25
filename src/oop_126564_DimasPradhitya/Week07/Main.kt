6712c2406248698d23ecdc8a084e8fec57916a9b
CryGord00 <dimaspradhitya73@gmail.com>
2026-03-25 15:58:10 +0700

    week07: (trial) non-exhaustive when compiler error

--- src/oop_126564_DimasPradhitya/Week07/Main.kt
+++ src/oop_126564_DimasPradhitya/Week07/Main.kt
@@ -8,4 +8,13 @@ fun main() {
     println("\n== TEST COMPANION OBJECT ===")
     val client = NetworkClient.createClient() // Instansiasi lewat Factory
     client.connect()
+
+    println("\n== TEST SEALED CLASS ===")
+    val response = ApiResponse = ApiResponse("Data berhabis ditarik")
+
+    //EROR: 'when' ecperssion must be exhaustive
+    val uiMassage = when (response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+    }
 }
