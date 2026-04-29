8eafcb33ac7db442b102633047174e118d0701f4
CryGord00 <dimaspradhitya73@gmail.com>
2026-04-29 15:52:18 +0700

    week10: (task) populate coin repository

--- src/oop_126564_DimasPradhitya/Week10/CryptoDashboard.kt
+++ src/oop_126564_DimasPradhitya/Week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_126564_DimasPradhitya.Week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
+
 }
