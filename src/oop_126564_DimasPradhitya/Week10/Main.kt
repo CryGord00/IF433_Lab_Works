67d2df0a0f9a228f63927562317a3ff1ad8d8ca1
CryGord00 <dimaspradhitya73@gmail.com>
2026-04-29 15:30:59 +0700

    week10: test generic functions with Double and String

--- src/oop_126564_DimasPradhitya/Week10/Main.kt
+++ src/oop_126564_DimasPradhitya/Week10/Main.kt
@@ -1,6 +1,6 @@
 package oop_126564_DimasPradhitya.Week10
 
-fun main() {
+fun main(processData: (String) -> Unit) {
     println("=== TEST GENERIC CLASS ===")
     val intBox = Box(100)
     val stringBox = Box("Generics in Kotlin");
@@ -11,4 +11,11 @@ fun main() {
     println("\n=== TEST MULTIPLE PARAMETERS ===")
     val itemPrice = PairBox("Bitcoin", 65000)
     println("ASET: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
+
+    println("\n=== TEST GENERIC FUNCTION ===")
+    printData(3.14)
+    val result = processData("Stable Coin")
+    println("Hasil Proses: $result")
 }
+
+
