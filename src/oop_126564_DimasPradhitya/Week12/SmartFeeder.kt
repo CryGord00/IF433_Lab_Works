f8d92c60618853338dff5ea4b50486dee3d8eee0
CryGord00 <dimaspradhitya73@gmail.com>
2026-05-13 09:34:06 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_126564_DimasPradhitya/Week12/SmartFeeder.kt
+++ src/oop_126564_DimasPradhitya/Week12/SmartFeeder.kt
@@ -3,4 +3,6 @@ package oop_126564_DimasPradhitya.Week12
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0)
     println("Porsi kibble harus lebih dari 0 gr")
+
+    if (isJammed) throw DispenserJamException()
 }
