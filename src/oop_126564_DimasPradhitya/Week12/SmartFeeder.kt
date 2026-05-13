bdd88bb0081b8a9b9edc1c4747817fb56d7eefda
CryGord00 <dimaspradhitya73@gmail.com>
2026-05-13 09:35:16 +0700

    week12: (task) setup SmartFeeder main function

--- src/oop_126564_DimasPradhitya/Week12/SmartFeeder.kt
+++ src/oop_126564_DimasPradhitya/Week12/SmartFeeder.kt
@@ -5,4 +5,9 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     println("Porsi kibble harus lebih dari 0 gr")
 
     if (isJammed) throw DispenserJamException()
+
+    if (requestedGram > availableGram) throw FoodEmptyException(requestedGram, availableGram)
+
+    println("Kibble berhasil dikeluarkan!")
+
 }
